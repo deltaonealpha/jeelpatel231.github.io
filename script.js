8b88c43fbba787ab4c6ce3cc0f9f3709899820ef
Jeel <33726155+JeelPatel231@users.noreply.github.com>
2020-06-07 02:59:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ function move(){
 	document.getElementById("nav-bottom").classList.toggle("expanded-nav-bottom");
 	document.getElementById("hamburger2").classList.toggle("expand-rotate");
 }
+
+function sidemenu(){
+	document.getElementById("sidemenu").classList.toggle("sidemenu-reveal");
+}
