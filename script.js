2f28423e43a7329ef736cccd429668fffacb032d
Jeel <33726155+JeelPatel231@users.noreply.github.com>
2020-08-21 19:58:53 +0530

    added darkmode toggle

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ function move(){
 function sidemenu(){
 	document.getElementById("sidemenu").classList.toggle("sidemenu-reveal");
 }
+
+function toggle(){
+
+	var allElements = document.getElementsByTagName("*");
+	var classes = [];
+
+	for (var i = 0, n = allElements.length; i < n; ++i) {
+	var el = allElements[i];
+	if (el.id) { classes.push(el.id); }
+	}
+
+	var i, len, text;
+	for (i = 0, len = classes.length, text = ""; i < len; i++) {
+ 		text = classes[i] + "_2";
+		document.getElementById(classes[i]).classList.toggle(text);
+		}
+}
