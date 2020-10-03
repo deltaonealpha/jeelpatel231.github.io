eb164a5ea2ebedaa2132fb9b90397fd7eed71b8e
jeelpatel231 <jeelpatel231@gmail.com>
2020-10-03 10:00:10 -0700

    added icons and :help:

--- test/script.js
+++ test/script.js
@@ -1,15 +1,20 @@
 function toggle_1() {
 	document.getElementById('textbox').style.transform = "translate(-10%,-50%)";
+	document.title = "Jeel | About Me";
 }
 function toggle_2() {
 	document.getElementById('textbox').style.transform = "translate(-30%,-50%)";
+	document.title = "Jeel | Contact";
 }
 function toggle_3() {
 	document.getElementById('textbox').style.transform = "translate(-50%,-50%)";
+	document.title = "Jeel | Home";
 }
 function toggle_4() {
 	document.getElementById('textbox').style.transform = "translate(-70%,-50%)";
+	document.title = "Jeel | My Works";
 }
 function toggle_5() {
 	document.getElementById('textbox').style.transform = "translate(-90%,-50%)";
+	document.title = "Jeel | :Help:";
 }
