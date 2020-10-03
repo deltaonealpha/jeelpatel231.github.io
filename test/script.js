34dbe75b70d49043f95e44315723ae8f4af09600
jeelpatel231 <jeelpatel231@gmail.com>
2020-10-03 08:20:08 -0700

    add a test page

---
+++ test/script.js
@@ -0,0 +1,15 @@
+function toggle_1() {
+	document.getElementById('textbox').style.transform = "translate(-10%,-50%)";
+}
+function toggle_2() {
+	document.getElementById('textbox').style.transform = "translate(-30%,-50%)";
+}
+function toggle_3() {
+	document.getElementById('textbox').style.transform = "translate(-50%,-50%)";
+}
+function toggle_4() {
+	document.getElementById('textbox').style.transform = "translate(-70%,-50%)";
+}
+function toggle_5() {
+	document.getElementById('textbox').style.transform = "translate(-90%,-50%)";
+}
